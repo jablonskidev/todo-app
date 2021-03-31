fad5b2ab28e53f3c4428dc21eb69a5911719cada
jablonskidev <36643503+jablonskidev@users.noreply.github.com>
2021-03-30 17:18:00 -0700

    Made sure the new task isn't an empty string

--- script.js
+++ script.js
@@ -7,15 +7,18 @@ function addTask(pressKey) {
   if(pressKey.keyCode === 13) {
     //Get the text that is inside the text input box
     let newTask = document.getElementById('input-task').value;
-    //Add that text to the list of tasks
-    let taskList = document.getElementById('task-list');
-    let newListItem = document.createElement('li');
-    newListItem.innerText = newTask;
-    taskList.appendChild(newListItem);
-    //Add an event listener for users removing tasks
-    newListItem.addEventListener('click', function(){     
-      //Create a function to remove a single task
-      taskList.removeChild(this);
-    })
+    //Make sure the user isn't inputting an empty string
+    if (newTask) {
+      //Add that text to the list of tasks
+      let taskList = document.getElementById('task-list');
+      let newListItem = document.createElement('li');
+      newListItem.innerText = newTask;
+      taskList.appendChild(newListItem);
+      //Add an event listener for users removing tasks
+      newListItem.addEventListener('click', function(){     
+        //Create a function to remove a single task
+        taskList.removeChild(this);
+      })
+    }  
   }  
 }
